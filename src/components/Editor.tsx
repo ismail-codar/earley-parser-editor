7a505f31ef3664a0653d973feb7fa3bf605f63e2
ismail codar <icodar@gmail.com>
2019-05-11 11:05:09 +0300

    ...

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,10 +1,33 @@
 import { Node } from "./Node";
-import { value } from "@fidanjs/runtime";
+import { value, FidanArray } from "@fidanjs/runtime";
 import { ROOT_NODE } from "../store";
 import { Graph } from "./webcola-graph/Graph";
+import { INode } from "../types";
+import { jsxArrayMap } from "../../.yalc/@fidanjs/jsx/build";
+
+const nodeList = (data: INode): INode[] => {
+  const nodes = [];
+
+  let childs = [data];
+  while (childs.length > 0) {
+    const child = childs.pop();
+    const text = child.text();
+    if (nodes.find(n => n.name === text) === undefined) {
+      nodes.push(child);
+      if (child.childs) {
+        childs.push.apply(childs, child.childs());
+      }
+    }
+  }
+  return nodes;
+};
 
 export const Editor = () => {
   console.log(JSON.parse(JSON.stringify(ROOT_NODE(), null, 1)));
+  const nodes = value(nodeList(ROOT_NODE())) as FidanArray<INode[]>;
+  // setTimeout(() => {
+  //   nodes()[5].text("YUKLEM_KOK...");
+  // }, 2000);
   return (
     <div>
       <Node text="Root" childsType={value("choice")} />
@@ -12,32 +35,20 @@ export const Editor = () => {
       <table>
         <thead>
           <tr>
-            <th>#</th>
             <th>Name</th>
-            <th>Position</th>
-            <th>Location</th>
           </tr>
         </thead>
-        <tbody>
-          <tr>
-            <td>1</td>
-            <td>Bob Dylan</td>
-            <td>Musician</td>
-            <td>California, USA</td>
-          </tr>
-          <tr>
-            <td>2</td>
-            <td>Eric Clapton</td>
-            <td>Musician</td>
-            <td>Ohio, USA</td>
-          </tr>
-          <tr>
-            <td>3</td>
-            <td>Daniel Kahneman</td>
-            <td>Psychologist</td>
-            <td>California, USA</td>
-          </tr>
-        </tbody>
+        <tbody
+          {...jsxArrayMap(
+            nodes,
+            node =>
+              (
+                <tr>
+                  <td>{node.text()}</td>
+                </tr>
+              ) as any
+          )}
+        />
       </table>
       <Graph />
     </div>
